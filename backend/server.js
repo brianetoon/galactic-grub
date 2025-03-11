128cf0487086993caf2ad1240d89fd2f9685b7a8
Brian Toon <brian.elliot.toon@gmail.com>
2025-03-11 13:06:46 -0600

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("hello from server.js")
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+const port = 8080;
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(port, () => {
+    console.log(`Listening for requets on port ${port}`)
+  });
+});
