76ab7414594e486efca0ab6992606c1a09e393d4
Brian Toon <brian.elliot.toon@gmail.com>
2025-03-14 10:28:30 -0600

    signing up & hasing passwords

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,13 +22,41 @@ const userSchema = new mongoose.Schema({
     trim: true,
     required: true,
     unique: true,
-    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/s,
   },
   avatarUrl: {
     type: String,
     trim: true,
   }
 }, { timestamps: true });
+
+// static signup method
+userSchema.statics.signup = async function (username, email, password) {
+  
+  // validation
+  if (!username || !email || !password) {
+    throw Error("Please fill in all required fields");
+  }
+  
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  // password hashing
+  const salt = await bcrypt.getSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // create new user
+  const user = await this.create({ username, email, password: hash});
+
+  return user;
+}
+
+
 
 const User = mongoose.model("User", userSchema);
 export default User;
