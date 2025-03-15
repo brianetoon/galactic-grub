0fb65d5b373e5994505e17c076e73955b6c19f46
Brian Toon <brian.elliot.toon@gmail.com>
2025-03-15 16:41:43 -0600

    initilize frontend with tailwind & shandcn ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-blue-900 text-3xl">Glactic Grub</h1>
+      <Button>Click me!</Button>
+    </div>
+  )
+}
+
+export default App
