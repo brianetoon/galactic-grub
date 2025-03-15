6a347d97c31c7f88c15115745ccec9cfd157db18
Brian Toon <brian.elliot.toon@gmail.com>
2025-03-14 20:24:17 -0600

    users controller

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,48 @@
+import User from "../models/user.js";
+import generateToken from "../utils/generateToken.js"
+
+export const signupUser = async (req, res, next) => {
+  console.log(`🚀 ${req.method} request for signing up a new user`);
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.signup(username, email, password);
+    const token = generateToken(user._id);
+
+    res.status(201).json({ 
+      success: true,
+      data: {
+        _id: user._id,
+        username: user.username,
+        email: user.email
+      },
+      token 
+    });
+
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const loginUser = async (req, res, next) => {
+  console.log(`🚀 ${req.method} request for logging in a user`);
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = generateToken(user._id);
+
+    res.status(201).json({ 
+      success: true,
+      data: {
+        _id: user._id,
+        username: user.username,
+        email: user.email
+      },
+      token 
+    });
+
+  } catch (err) {
+    next(err);
+  }
+}
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,53 +1,12 @@
 import express from "express";
-import User from "../models/user.js";
-import generateToken from "../utils/generateToken.js";
+import {
+  loginUser,
+  signupUser
+} from "../controllers/usersController.js";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res, next) => {
-  console.log(`🚀 ${req.method} request for signing up a new user`);
-  const { username, email, password } = req.body;
-
-  try {
-    const user = await User.signup(username, email, password);
-    const token = generateToken(user._id);
-
-    res.status(201).json({ 
-      success: true,
-      data: {
-        _id: user._id,
-        username: user.username,
-        email: user.email
-      },
-      token 
-    });
-
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post("/login", async (req, res, next) => {
-  console.log(`🚀 ${req.method} request for logging in a user`);
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.login(email, password);
-    const token = generateToken(user._id);
-
-    res.status(201).json({ 
-      success: true,
-      data: {
-        _id: user._id,
-        username: user.username,
-        email: user.email
-      },
-      token 
-    });
-
-  } catch (err) {
-    next(err);
-  }
-});
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
 
 export default router;
